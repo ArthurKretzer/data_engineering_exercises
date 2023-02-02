869453af7a19e25404ee7c5fa3eb52b3dbdd2e0c
Arthur Raulino Kretzer <arthur.raulino.kretzer@gmail.com>
2023-02-02 20:24:05 -0300

    Added a better logger to .py

--- Exercise 2/solution.py
+++ Exercise 2/solution.py
@@ -159,4 +159,147 @@ test_func()
 # %%
 test_func(10, name="testing")
 
+# %%
+import logging
+
+# %% [markdown]
+# Creates a logger with level DEBUG and a formatter for the logging message
+
+# %%
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+# %% [markdown]
+# Now creates a stream handler for the logging messages using the declared formatter and sets it as the log handler
+
+# %%
+ch = logging.StreamHandler()
+ch.setFormatter(formatter)
+log.addHandler(ch)
+
+# %% [markdown]
+# Now we can use it instead of print to give meaningful messages
+
+# %%
+import random
+
+#This receives a delay between tries, expo defines an exponential delay, then a list of exception triggers for backoff, then maximum tryouts.
+@backoff.on_exception(backoff.expo, (ConnectionAbortedError, ConnectionRefusedError, TimeoutError), max_tries=10)
+def test_func(*args, **kargs):
+    rnd = random.random()
+    log.debug(f"RND: {rnd}")
+    log.info(f"args: {args if args else 'no args'}")
+    log.info(f"kargs: {kargs if kargs else 'no kargs'}")
+
+    if rnd < .2:
+        log.error('Connection terminated.')
+        raise ConnectionAbortedError('Connection terminated.')
+    if rnd < .4:
+        log.error('Connection refused.')
+        raise ConnectionRefusedError('Connection refused.')
+    if rnd < .6:
+        log.error('Connection timeout.')
+        raise TimeoutError('Connection timeout.')
+    else:
+        return "OK!"
+
+# %%
+test_func()
+
+# %% [markdown]
+# My log formatting.
+# 
+# This implementation has colors, bold font and also process, thread, timestamp, module and line logging.
+# 
+# This is also saved on a .log file automatically
+
+# %%
+import logging
+import os
+# Color number definition
+BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
+
+# These are the sequences need to get colored output
+RESET_SEQ = "\033[0m"
+COLOR_SEQ = "\033[1;%dm"
+BOLD_SEQ = "\033[1m"
+
+COLORS = {
+    'WARNING': YELLOW,
+    'INFO': WHITE,
+    'DEBUG': BLUE,
+    'CRITICAL': RED,
+    'ERROR': RED
+}
+
+# Special function used to ease a message formatting edition
+
+
+def formatter_message(message, use_color=True):
+    if use_color:
+        message = message.replace(
+            "$RESET", RESET_SEQ).replace("$BOLD", BOLD_SEQ)
+    else:
+        message = message.replace("$RESET", "").replace("$BOLD", "")
+    return message
+
+# Format log level name color accordinly
+
+
+class ColoredFormatter(logging.Formatter):
+    def __init__(self, msg, use_color=True):
+        logging.Formatter.__init__(self, msg)
+        self.use_color = use_color
+
+    def format(self, record):
+        levelname = record.levelname
+        if self.use_color and levelname in COLORS:
+            levelname_color = COLOR_SEQ % (
+                30 + COLORS[levelname]) + levelname + RESET_SEQ
+            record.levelname = levelname_color
+        return logging.Formatter.format(self, record)
+
+# Logger class used in all logging operations
+
+
+class log(logging.Logger):
+    # Message format with collors \033[1;35m = Magenta
+    FORMAT = '\033[35m%(asctime)s\033[0m [$BOLD%(levelname)-18s$RESET]\033[35m [%(processName)s][%(threadName)s][%(module)s]\033[0m %(message)s - $BOLDLine:%(lineno)d$RESET'
+    COLOR_FORMAT = formatter_message(FORMAT, True)
+
+    def __init__(self, name='my_logger'):
+        # Create logger with debug level
+        logging.Logger.__init__(self, name, logging.DEBUG)
+
+        # create console handler and set level to debug
+        if not self.handlers:
+            color_formatter = ColoredFormatter(self.COLOR_FORMAT)
+            ch = logging.StreamHandler()
+            ch.setLevel(logging.DEBUG)
+            # add formatter to ch
+            ch.setFormatter(color_formatter)
+            # add ch to logger
+            self.addHandler(ch)
+
+            log_file = logging.FileHandler(
+                filename=os.getcwd()+f'/logs/{name}.log', mode='w+', encoding='utf8')
+            formatter = logging.Formatter(
+                '%(asctime)s [%(levelname)-18s][%(processName)s][%(threadName)s][%(module)s] %(message)s - %(lineno)d')
+            log_file.setFormatter(formatter)
+            log_file.setLevel(logging.DEBUG)
+            self.addHandler(log_file)
+
+
+# %%
+log = log('my_log')
+
+# %%
+test_func()
+
+# %%
+
+
 
