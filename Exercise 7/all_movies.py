2acd6db925bc4f940c3b61cc436bb392e18f4b6b
Arthur Kretzer <arthur.raulino.kretzer@gmail.com>
2023-02-20 15:33:12 -0300

    Included screenshot of nicholas cage

--- Exercise 7/all_movies.py
+++ Exercise 7/all_movies.py
@@ -2,10 +2,22 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pandas as pd
 
+# import Action chains 
+from selenium.webdriver.common.action_chains import ActionChains
+
 driver = webdriver.Chrome()
 driver.get('https://pt.wikipedia.org/wiki/Nicolas_Cage')
 
 tabela = driver.find_element(By.XPATH, '//*[@id="mw-content-text"]/div[1]/table[2]')
+
+with open('print.png', 'wb') as f:
+    # create action chain object
+    action = ActionChains(driver)
+    # Scrolls down the page to full visualize nicholas cage photo. This prevents cropping.
+    driver.execute_script("window.scrollTo(0,300)")
+    nicholas_cage_photo = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[3]/main/div[2]/div[3]/div[1]/table[1]/tbody/tr[2]/td/div/div/div/a/img')
+    # Takes screenshow as png and writes directly on the file
+    f.write(nicholas_cage_photo.screenshot_as_png)
 
 # In oder to read_html interpret the html as a table,
 #  we need to include tags at the start and the end of the html.
